1abb7e3d0ea580a98f5dbfd1af9ed7095e4c5c3d
Lucas Silva <lucas.silvaaaa30@gmail.com>
2025-10-22 09:30:38 -0300

    feat(web): add tanstack router

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,25 +1,22 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
-import { fileURLToPath, URL } from 'node:url'
-import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    nitroV2Plugin(),
     tanstackRouter({
-      target: 'react',
+      target: "react",
       autoCodeSplitting: true,
+      generatedRouteTree: "./src/routeTree.gen.ts",
+      routesDirectory: "./src/pages",
+      routeToken: "layout",
     }),
-    viteReact(),
+    react(),
     tailwindcss(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
+  server: {
+    host: true,
+    port: 3000,
   },
-})
+});
